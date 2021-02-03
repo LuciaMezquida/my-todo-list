161ce8166bb54baaecdbe18f6bbd71cb7132b853
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-03 18:06:24 +0100

    Select day, month, year and week day from the DOM and put the current date

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
-'use strict';
+"use strict";
+let day = document.querySelector(".js-day");
+let month = document.querySelector(".js-month");
+let year = document.querySelector(".js-year");
+let weekDay = document.querySelector(".js-weekDay");
+const listContainer = document.querySelector(".js-list");
+const addButon = document.querySelector(".js-btn");
 
-console.log('>> Ready :)');
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const weekDays = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+];
+
+const date = new Date();
+day.innerHTML = date.getDate();
+month.innerHTML = months[date.getMonth()];
+year.innerHTML = date.getFullYear();
+weekDay.innerHTML = weekDays[date.getDay()];
+console.log(date);
+console.log(day);
