fa30f63cf5346dc231374a101f3460818fdc0859
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-11 15:27:20 +0100

    Refactor code

--- src/js/03-create-tasks-list.js
+++ src/js/03-create-tasks-list.js
@@ -3,15 +3,18 @@
 const createElements = () => {
   if (inputValue !== "") {
     const checkbox = document.createElement("input");
-    const customCheckbox = document.createElement("span");
-    const checkboxLabel = document.createElement("label");
     checkbox.classList.add("checkbox");
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("name", inputValue);
+
+    const customCheckbox = document.createElement("span");
     customCheckbox.classList.add("custom-check");
     customCheckbox.setAttribute("name", inputValue);
+
+    const checkboxLabel = document.createElement("label");
     checkboxLabel.classList.add("task-label");
     checkboxLabel.innerHTML = inputValue;
+
     addTaskToTheList(checkbox, customCheckbox, checkboxLabel);
     printList();
     deleteInputAndForm();
